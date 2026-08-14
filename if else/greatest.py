c3ed3f0c881bd0de0a9d7abc25ce1ad0e437391b
Ameymunse04 <ameymunse004@gmail.com>
2026-08-14 15:37:58 +0530

    Add files via upload

---
+++ if else/greatest.py
@@ -0,0 +1,6 @@
+n1 = int(input("Enter number n1:"))
+n2 = int(input("Enter number n2:"))
+if n1>=n2:
+    print("Number n1 is greatest", n1)
+else:
+    print("number n2 is greatest: ", n2)
---
+++ if else/positive.py
@@ -0,0 +1,8 @@
+n = int(input("Enter number n: "))
+
+if n >= 1:
+    print("Number is positive")
+elif n < 0:
+    print("Number is negative")
+else:
+    print("Number is zero")
